6664f474719730af09640596e882f0f31fe408dc
Nancy1989-Tenai <nancietenai@gmail.com>
2026-09-17 15:11:36 +0300

    Add greet function and integrate into main program

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+def square(n):
+    """Returns the square of a number."""
+    return n ** 2
+
+def is_even(n):
+    """Returns True if a number is even, False otherwise."""
+    return n % 2 == 0
+
+def celsius_to_fahrenheit(c):
+    """Converts Celsius to Fahrenheit using F = (C * 9/5) + 32."""
+    return (c * 9/5) + 32
+
+def greet(name):
+    """Returns a personalized greeting string."""
+    return f"Hello, {name}! Welcome to Python Lab."
